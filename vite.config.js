a6ff3d06492e0d80d95849f11707e33cd7732a8d
Mirosmar Oliveira <contatomirosmaroliveira@hotmail.com>
2026-05-11 23:56:42 -0300

    feat: initialize App with react-router-dom and configure GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/me/',   // ← nome do repositório no GitHub Pages
   plugins: [react()],
   resolve: {
     alias: {
